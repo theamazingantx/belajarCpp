ea0dc89d8fe8d05323196dac85240ecef8005af9
Antx★ <148527164+theamazingantx@users.noreply.github.com>
2024-10-13 21:34:39 +0700

    Create halodunia.cpp
    Selain membuat terdapat kode, dan analogi yang saya pahami

---
+++ halodunia.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // mengambil file perpustakaan iostream
+using namespace std; // otomatis gunakan kata std
+
+int main() // fungsi utama/yang akan dijalankan terlebih dahulu
+{
+cout << "halo" << endl; // perintah untuk mencetak isi ke terminal [ cout= console out, endl= end line ]
+return 0; // kalo berhasil bakal muncul 0
+}
+
